171c0609a204c0eeea7ee58f0478882d2d6e3c96
montalokmane <43132615+montalokmane@users.noreply.github.com>
2018-10-15 13:20:18 +0300

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,19 @@
+def thing():
+    print('Hello')
+    print('Fun')
+thing()
+print('Zip')
+thing()
+
+print(" Rudens")
+
+def max(inp):
+    blah
+    for x in inp:
+        blah
+def addtwo(a, b):
+    added = a + b
+    return added
+x = addtwo(3, 5)
+print(x)
+
