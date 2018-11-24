00d68c7c76e135655c3e046f223426c07a9559f7
montalokmane <43132615+montalokmane@users.noreply.github.com>
2018-11-24 12:41:39 +0200

    Add files via upload

---
+++ 20181124_12_40.py
@@ -0,0 +1,25 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/Lietotajs/Desktop/Skolas darbi/Datormācība/gala versija.py 
+sin aprēķināšana:
+Ievadi argumentu x: 0.24
+sin(0.24)=0.06
+a0 = 0.06 S0 = 0.06
+a36 = 0.00 S36 = 0.06
+a100 = 0.00 S100 = 0.06
+sin (0.24) caur summu: 0.06
+           100 
+        -------- 
+        \                 k     4*k+2 
+         \            (-1)   * x 
+sin(0.24)= >       ---------------------- 
+         /                 (2*k+1)! 
+        / 
+        -------- 
+          k = 0 
+                                   4 
+                           (-1) * x 
+Rekurences reizinātājs:  ----------------- 
+                           (4*k)*(4*k+1) 
+>>> 
