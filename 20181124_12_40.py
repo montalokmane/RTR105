72cb5eb062f4ccda929cb927efb30e30e11f7404
montalokmane <43132615+montalokmane@users.noreply.github.com>
2018-11-24 12:42:51 +0200

    Update 20181124_12_40.py

--- 20181124_12_40.py
+++ 20181124_12_40.py
@@ -1,7 +1,4 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
- RESTART: C:/Users/Lietotajs/Desktop/Skolas darbi/Datormācība/gala versija.py 
+
 sin aprēķināšana:
 Ievadi argumentu x: 0.24
 sin(0.24)=0.06
