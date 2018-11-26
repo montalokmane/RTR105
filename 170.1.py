a8fad81b3f2d22d677112c4968f48f7dab96f498
montalokmane <43132615+montalokmane@users.noreply.github.com>
2018-11-26 13:52:06 +0200

    Add files via upload

---
+++ 170.1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from math import sin, fabs
+from time import sleep
+
+def f(x):
+	return sin(x)
+
+a = 1.1
+b = 3.2
+ 
+funa = f(a)
+funb = f(b)
+
+if (funa * funb > 0.0 ):	
+    print " Dotajā intervālā [%s, %s] Sakņu nav " %(a,b)
+    sleep(1); exit()
+else:
+    print "Dotajā intervālā sakne(s) ir!"
+
+deltax = 0.01
+
+while ( fabs (b-a) > deltax ) :
+    x = (a+b)/2; funx = f(x)
+    if (funa * funx < 0. ) :
+        b = x
+    else:
+        a = x
+
+print " Sakne is : ", x
